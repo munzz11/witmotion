9fe19e48e7f6441b1e727426f3bf12461303fac4
Roland Arsenault <roland@ccom.unh.edu>
2022-07-12 15:09:14 -0400

    Close the bracket

--- scripts/set_witmotion_to_defaults.py
+++ scripts/set_witmotion_to_defaults.py
@@ -7,6 +7,6 @@ import struct
 if len(sys.argv) != 3:
     print("usage: set_witmotion_to_defaults.py port baud (ex: set_witmotion_to_defaults.py /dev/ttyUSB0 115200)")
 else:
-    s = serial.Serial(sys.argv[1], int(sys.argv[2])
+    s = serial.Serial(sys.argv[1], int(sys.argv[2]))
     data = struct.pack('<5B', 0xff, 0xaa, 0x00, 0x01, 0x00)
     s.write(data)
